adf98da16949d9e58e6c44fed2efe205381183bb
Netra9686 <naitravathi.javaventures@gmail.com>
2022-09-19 12:33:54 -0400

    check even_odd number

--- even.py
+++ even.py
@@ -1,7 +1,7 @@
-def even():
+def even_odd():
     num = int(input('Enter the number: '))
     if (num % 2) == 0:
         print('Even number')
     else:
         print('Odd number')
-even()
+even_odd()
